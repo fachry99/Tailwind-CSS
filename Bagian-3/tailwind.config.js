5f3f5038e064fd2e782353d5b03e14bdc4ecc7e2
fachry99 <151fachry@gmail.com>
2023-10-21 10:37:33 +0700

    update

---
+++ Bagian-3/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class", // or 'media' or 'class' or false
+  theme: {
+    extend: { spacing: { 13: "3.25rem" } },
+  },
+  plugins: [],
+};
